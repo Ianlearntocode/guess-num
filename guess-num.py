23696a027b1cc8b21e5c59d2e547bc79263005bf
Ian Liu <a8032571@gmail.com>
2022-02-04 12:20:45 +0800

    final verison

--- guess-num.py
+++ guess-num.py
@@ -1,10 +1,14 @@
 import random
+start = input('請決定雖機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1,100)
+r = random.randint(start,end)
 count = 0
 while True:
     count += 1 # count = count + 1
-    num = input('請輸入猜測的數字(1~100): ')
+    num = input('請輸入猜測的數字: ')
     num = int(num)
     if num == r:
         print('你猜中了!')
